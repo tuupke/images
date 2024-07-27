88e59d878b62910437b1b99cfc3913a205273554
Mart Pluijmaekers <mart@tuupke.nl>
2024-07-27 14:03:24 +0200

    Fixed sorting and can now open video files too

--- images.go
+++ images.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"math/rand/v2"
 	"net/http"
@@ -10,9 +11,35 @@ import (
 	"strconv"
 	"strings"
 	"sync"
+	"time"
 )
 
+var (
+	videoProgram = env("VIDEO_PROGRAM", "vlc")
+	imageProgram = env("IMAGE_PROGRAM", "imv")
+
+	programToMime = map[string][]string{
+		videoProgram: {"video/", "application/octet-stream"},
+		imageProgram: {"image/"},
+	}
+
+	program = imageProgram
+	start   = time.Now()
+)
+
+func env(key, fb string) string {
+	if v := os.Getenv(key); v != "" {
+		return v
+	}
+
+	return fb
+}
+
 func main() {
+	if strings.HasPrefix(os.Args[0], "video") {
+		program = "vlc"
+	}
+
 	path, err := os.Getwd()
 	if err != nil {
 		panic(err)
@@ -23,33 +50,7 @@ func main() {
 		recurse = 0
 	}
 
-	entries, err := contents(path, recurse)
-	if err != nil {
-		panic(err)
-	}
-
-	if s, ok := os.LookupEnv("SORT"); !ok || s == "random" {
-		rand.Shuffle(len(entries), func(i, j int) { entries[i], entries[j] = entries[j], entries[i] })
-	} else if strings.HasSuffix(s, "time") {
-		sort.Slice(entries, func(i, j int) bool {
-			return entries[i].ModTime().Before(entries[j].ModTime())
-		})
-	} else if strings.HasPrefix(s, "new") {
-		sort.Slice(entries, func(i, j int) bool {
-			return entries[j].ModTime().Before(entries[i].ModTime())
-		})
-	}
-
-	if limitString := os.Getenv("LIMIT"); limitString != "" {
-		if limit, err := strconv.Atoi(limitString); err == nil {
-			limit = min(limit, len(entries))
-			fmt.Println("Limiting to", limit)
-			entries = entries[:limit]
-		}
-	}
-
-	var send, receive = make(chan string, 1), make(chan string, 1)
-
+	var send, receive = make(chan fullEntry), make(chan fullEntry)
 	var wg = new(sync.WaitGroup)
 
 	numWorkers := 16
@@ -58,30 +59,71 @@ func main() {
 		go mimeWorker(wg, send, receive)
 	}
 
-	files := make([]string, 0, len(entries))
+	// Prepare the receiving of files.
+	entries := make([]fullEntry, 0, 100000)
 
+	// Close the receiving channel after checking the entire directory.
 	go func() {
 		wg.Wait()
 		close(receive)
 	}()
 
+	// Rad the results
 	go func() {
-		defer close(send)
-		for _, entry := range entries {
-			if !entry.IsDir() {
-				send <- entry.path
-			}
+		for entry := range receive {
+			entries = append(entries, entry)
 		}
 	}()
 
-	for entry := range receive {
-		files = append(files, entry)
+	// Retrieve all contents of the directory.
+	if err := contents(path, recurse, send); err != nil {
+		panic(err)
 	}
 
-	fmt.Println("Opening", len(files), "files")
-	files = append(os.Args[1:], files...)
+	// Sent all files, close the sending channel.
+	close(send)
 
-	fmt.Println(exec.Command("imv", files...).Run())
+	if s, ok := os.LookupEnv("SORT"); !ok || s == "random" {
+		fmt.Println("Sorting randomly")
+		rand.Shuffle(len(entries), func(i, j int) { entries[i], entries[j] = entries[j], entries[i] })
+	} else if strings.HasSuffix(s, "time") {
+		fmt.Println("Sorting by oldest first")
+		sort.Slice(entries, func(i, j int) bool {
+			return entries[i].ModTime().Before(entries[j].ModTime())
+		})
+	} else if strings.HasPrefix(s, "new") {
+		fmt.Println("Sorting by newest first")
+		sort.Slice(entries, func(i, j int) bool {
+			return entries[j].ModTime().Before(entries[i].ModTime())
+		})
+	}
+
+	var upperBound = len(entries)
+	if limitString := os.Getenv("LIMIT"); limitString != "" {
+		if limit, err := strconv.Atoi(limitString); err == nil {
+			upperBound = min(limit, upperBound)
+		}
+	}
+
+	files := make([]string, upperBound)
+	for i := range upperBound {
+		files[i] = entries[i].path
+	}
+
+	fmt.Printf("Found %v files, took %v. Will open files using '%v'.\n", len(files), time.Since(start), program)
+	if len(files) == 0 {
+		fmt.Println("No files to open, exiting.")
+		return
+	}
+
+	files = append(os.Args[1:], files...)
+	if err := exec.Command(program, files...).Run(); err != nil {
+		fmt.Println("Error running", program)
+		var exitErr exec.ExitError
+		if errors.As(err, &err) {
+			os.Exit(exitErr.ExitCode())
+		}
+	}
 }
 
 type fullEntry struct {
@@ -89,62 +131,61 @@ type fullEntry struct {
 	path string
 }
 
-func contents(pwd string, recurse int) ([]fullEntry, error) {
+func contents(pwd string, recurse int, send chan<- fullEntry) error {
 	entries, err := os.ReadDir(pwd)
 	if err != nil {
-		return nil, err
+		return err
 	}
 
-	var allEntries = make([]fullEntry, 0, len(entries))
 	for _, entry := range entries {
 		if entry.IsDir() {
 			if recurse == 0 {
 				continue
 			}
 
-			dirEntries, err := contents(pwd+"/"+entry.Name(), recurse-1)
-			if err != nil {
-				return allEntries, nil
+			if err := contents(pwd+"/"+entry.Name(), recurse-1, send); err != nil {
+				return err
 			}
-
-			allEntries = append(allEntries, dirEntries...)
 		}
 
 		info, err := entry.Info()
 		if err != nil {
-			return allEntries, err
+			return err
 		}
 
-		allEntries = append(allEntries, fullEntry{
+		send <- fullEntry{
 			FileInfo: info,
 			path:     pwd + "/" + entry.Name(),
-		})
+		}
 	}
 
-	return allEntries, nil
+	return nil
 }
 
 // mimeWorker reads paths from a channel and tries to detect the mimetype. Emits
 // the path on a different channel if the mimetype is an image or detection
 // fails.
-func mimeWorker(wg *sync.WaitGroup, read <-chan string, write chan<- string) {
+func mimeWorker(wg *sync.WaitGroup, read <-chan fullEntry, write chan<- fullEntry) {
 	defer wg.Done()
 
 	// The content type exists in the first 512 bytes.
 	buf := make([]byte, 512)
 
-	for path := range read {
-		output, err := os.Open(path)
+outer:
+	for fe := range read {
+		output, err := os.Open(fe.path)
 		if err == nil {
 			_, err = output.Read(buf)
-			output.Close()
+			_ = output.Close()
 
 			contentType := http.DetectContentType(buf)
-			if err == nil && !strings.HasPrefix(contentType, "image/") {
-				continue
+
+			for _, mime := range programToMime[program] {
+				if err == nil && strings.HasPrefix(contentType, mime) {
+					write <- fe
+					continue outer
+				}
 			}
 		}
-
-		write <- path
 	}
 }
